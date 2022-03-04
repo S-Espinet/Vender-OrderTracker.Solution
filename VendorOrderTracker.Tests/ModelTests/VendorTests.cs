86886ff24e034293e10df456daa272786df67fc5
S. Espinet <s.lou.espinet@gmail.com>
2022-03-04 13:16:59 -0800

    pass GetVendorName test

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -10,8 +10,22 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor();
+      Vendor newVendor = new Vendor("test vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetVendorName_ReturnsVendorName_String()
+    {
+      //Arrange
+      string vendorName = "Test Vendor";
+      Vendor newVendor = new Vendor(vendorName);
+
+      //Act
+      string result = newVendor.VendorName;
+
+      //Assert
+      Assert.AreEqual(vendorName, result);
     }
   }
 }
--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -4,10 +4,12 @@ namespace VendorOrderTracker.Models
 {
   public class Vendor
   {
-
-    public Vendor()
+    private static List<Vendor> _instances = new List<Vendor>{};
+    public string VendorName { get; set; }
+    
+    public Vendor(string vendorName)
     {
-
+      VendorName = vendorName;
     }
   }
 }
